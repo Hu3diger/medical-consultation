6168eb2e9f80ee66698cc5cce8a5e45a2519ca2a
Gabriel Ayala <gabriel.molinas@catolicasc.org.br>
2019-06-04 21:58:56 -0300

    #2 - Finalizando tela

--- app/src/main/java/br/org/catolicasc/gmedicalcare/agendar_consulta.java
+++ app/src/main/java/br/org/catolicasc/gmedicalcare/agendar_consulta.java
@@ -2,12 +2,19 @@ package br.org.catolicasc.gmedicalcare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class agendar_consulta extends AppCompatActivity {
+
+    private EditText dataConsulta;
+    private EditText horarioConsulta;
+    private EditText sintomas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar_consulta);
+
+
     }
 }
