2761bcd5ef3d2834ce7f98dbf6c12a89924579e6
douglaswagner <martinruediger2010@gmail.com>
2019-06-11 19:17:53 -0300

    #8 - Finalizada a estruturação

--- app/src/main/java/br/org/catolicasc/gmedicalcare/ConsultasFragment.java
+++ app/src/main/java/br/org/catolicasc/gmedicalcare/ConsultasFragment.java
@@ -7,16 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ConsultasFragment extends Fragment {
-
-    private static final String TAG = "TESTE 2";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.consultas_fragment, container, false);
-
         return view;
     }
 }
