6560611a1ad1be1c89016103d20fcbd1db0316ea
Martin Ruediger <martinr@weg.net>
2019-06-21 10:25:59 -0300

    #9 - Iniciando implementação

--- app/src/main/java/br/org/catolicasc/gmedicalcare/AgendarConsulta.java
+++ app/src/main/java/br/org/catolicasc/gmedicalcare/AgendarConsulta.java
@@ -1,5 +1,6 @@
 package br.org.catolicasc.gmedicalcare;
 
+import android.app.DatePickerDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
@@ -7,23 +8,95 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class AgendarConsulta extends AppCompatActivity {
 
+    // Projection array. Creating indices for this array instead of doing
+    // dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[]{
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
     // The indices for the projection array above.
     private static final int PROJECTION_ID_INDEX = 0;
     private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
     private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
     private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-
+    final Calendar myCalendar = Calendar.getInstance();
+    private EditText sintomas, dataConsulta;
+    private Spinner spinner;
+    private Button btnSchedule, btnDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendar_consulta);
+        btnSchedule = findViewById(R.id.btnSchedule);
+        spinner = findViewById(R.id.spinner);
+        sintomas = findViewById(R.id.sintomas);
+        dataConsulta = findViewById(R.id.data_consulta);
+        btnDate = findViewById(R.id.btnDate);
+        dataConsulta.setEnabled(false);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.teste, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        btnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AgendarConsulta.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        btnSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callCalendar();
+            }
+        });
+    }
+
+    private void updateLabel() {
+
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, new Locale("pt", "BR"));
+
+        dataConsulta.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    public void callCalendar() {
+
+        System.out.println(myCalendar.get(Calendar.DAY_OF_MONTH));
         // Run query
         Cursor cur = null;
         ContentResolver cr = getContentResolver();
@@ -31,7 +104,7 @@ public class AgendarConsulta extends AppCompatActivity {
         String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
                 + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
                 + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-        String[] selectionArgs = new String[] {"martinruediger2010@gmail.com", "com.google",
+        String[] selectionArgs = new String[]{"martinruediger2010@gmail.com", "com.google",
                 "martinruediger2010@gmail.com"};
         // Submit the query and get a Cursor object back.
         cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
@@ -47,17 +120,12 @@ public class AgendarConsulta extends AppCompatActivity {
             displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
             accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
             ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
-
-            // Do something with the values...
-            System.out.println("ID:" + calID + " dispNAME:" +displayName + " accNAME:" +accountName + " OWNER:" +ownerName);
         }
 
         insertEvent();
-
-
     }
 
-    public void insertEvent(){
+    public void insertEvent() {
         Calendar beginTime = Calendar.getInstance();
         beginTime.set(2012, 0, 19, 7, 30);
         Calendar endTime = Calendar.getInstance();
@@ -66,23 +134,13 @@ public class AgendarConsulta extends AppCompatActivity {
                 .setData(CalendarContract.Events.CONTENT_URI)
                 .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
                 .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
-                .putExtra(CalendarContract.Events.TITLE, "Yoga")
-                .putExtra(CalendarContract.Events.DESCRIPTION, "Group class")
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, "The gym")
-                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
-                .putExtra(Intent.EXTRA_EMAIL, "rowan@example.com,trevor@example.com");
+                .putExtra(CalendarContract.Events.TITLE, "Conulta Médica - Dr. José Alface")
+                .putExtra(CalendarContract.Events.DESCRIPTION, "Sintomas: " + sintomas.getText())
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "Clínica Alface's - Rua José Teodoro Brasileiro, 2574, Sala 2")
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY);
 
         startActivity(intent);
 
     }
-
-    // Projection array. Creating indices for this array instead of doing
-    // dynamic lookups improves performance.
-    public static final String[] EVENT_PROJECTION = new String[]{
-            CalendarContract.Calendars._ID,                           // 0
-            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
-    };
 
 }
